a18cb905854f3463a34a1830f03d3a440e1c2144
Jonathan Roth <jonathan_roth@brown.edu>
2023-04-17 15:05:56 -0400

    Updated weighting fucntions

--- R/compute_bounds_ats.R
+++ R/compute_bounds_ats.R
@@ -4,6 +4,7 @@
 #' @param d Name of the treatment variable in the df
 #' @param m Name of the mediator variable
 #' @param y Name of the outcome variable
+#' @param w (Optional) Name of weighting variable. If null, equal weights are used
 #' @importFrom "stats" "quantile"
 #' @export
 
@@ -19,6 +20,7 @@ compute_bounds_ats <- function(df, d, m, y, w = NULL){
     wvec <- df[[w]]
   }
 
+
   #ATs Y(0,1) outcome is point-identified based on observations with D=0,M=1
   ats_untreated_index <- (dvec == 0) & (mvec == 1)
   ats_untreated_mean <- stats::weighted.mean( x = yvec[ats_untreated_index],
@@ -28,25 +30,24 @@ compute_bounds_ats <- function(df, d, m, y, w = NULL){
   ats_treated_index <- (dvec == 1) & (mvec == 1) #these are ATs or Cs
 
   #Compute fraction of ATs/Cs
-  frac_compliers <- stats::weighted.mean( mvec[dvec == 1], w =  wvec[dvec == 1] ) -
-    stats::weighted.mean( mvec[dvec == 0], w = wvec[dvec == 0] )
-  frac_ats <- stats::weighted.mean( mvec[ dvec == 0 ], w = wvec[ dvec == 0] )
+  frac_compliers <- stats::weighted.mean( x = mvec[dvec == 1], w = wvec[dvec == 1] ) -
+    stats::weighted.mean( x = mvec[dvec == 0], w = wvec[dvec == 0] )
+  frac_ats <- stats::weighted.mean( x = mvec[ dvec == 0 ],  w = wvec[ dvec == 0 ]  )
   theta_ats <- frac_ats / (frac_compliers + frac_ats) #fraction among Cs/ATs
 
   # Get ub on Y(1,1) by trimming to the upper theta_at's fraction
-  quantile_ub <- Hmisc::wtd.quantile(x = yvec[ats_treated_index],
-                                     probs = 1-theta_ats,
-                                     weights = wvec[ats_treated_index],
-                                     normwt = T)
+  quantile_ub <- reldist::wtd.quantile(x = yvec[ats_treated_index],
+                                       q = 1-theta_ats,
+                                       weight = wvec[ats_treated_index])
 
-  ub_treated_mean <- stats::weighted.mean( x = yvec[ ats_treated_index & yvec >= quantile_ub  ],
-                                           w = wvec[ ats_treated_index & yvec >= quantile_ub  ])
+  ub_treated_mean <- stats::weighted.mean( yvec[ ats_treated_index & yvec >= quantile_ub  ],
+                                           wvec[ ats_treated_index & yvec >= quantile_ub  ])
 
   # Get lb on Y(1,1) by trimming to the lower theta_at's fraction
-  quantile_lb <- Hmisc::wtd.quantile(x = yvec[ats_treated_index],
-                                     probs = theta_ats,
-                                     weights = wvec[ats_treated_index],
-                                     normwt = T)
+  quantile_lb <- reldist::wtd.quantile(x = yvec[ats_treated_index],
+                                       q = theta_ats,
+                                       weight = wvec[ats_treated_index])
+
   lb_treated_mean <- stats::weighted.mean( x = yvec[ ats_treated_index & yvec <= quantile_lb  ],
                                            w = wvec[ ats_treated_index & yvec <= quantile_lb  ])
 
