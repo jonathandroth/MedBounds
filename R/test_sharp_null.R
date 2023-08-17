7ccd1d62f6d2c143ca359ddab075d6b668bf4b31
Soonwoo Kwon <soonwoo.kn@gmail.com>
2023-08-17 16:39:04 -0400

    Updated the sharp null test

--- R/test_sharp_null.R
+++ R/test_sharp_null.R
@@ -14,7 +14,7 @@
 #'   ordering is used.
 #' @param B Bootstrap size, default is zero
 #' @param weight.matrix Weight matrix used to implement FSST. Possible options
-#'   are "diag", "avar", "identity"
+#'   are "diag", "avar", "identity." Defaults is "diag" as in FSST.
 #' @export
 test_sharp_null <- function(df,
                             d,
@@ -22,7 +22,7 @@ test_sharp_null <- function(df,
                             y,
                             ordering = NULL,
                             B = 500,
-                            weight.matrix = NULL){
+                            weight.matrix = "diag"){
 
   yvec <- df[[y]]
   dvec <- df[[d]]
@@ -45,13 +45,11 @@ test_sharp_null <- function(df,
   # Getting ready to use lpinfer
   # x = (theta, delta, zeta, kappa, eta)
   # zeta, kappa: nuisance par to convert inequalities to equalities
-  # eta: nuisance par to create target par
+  # eta: nuisance par to create target par (= theta_kk TV_kk)
 
   par_lengths <- c("theta" = K^2, "delta" = d_y * K, "zeta" = K,
                    "kappa" = d_y * K, "eta" = K)
   len_x <- sum(par_lengths)
-
-  # Set shape constraints using A.shp x = beta.shp
 
   # Set theta_lk = 0 for l > k using
   # Matrix that encodes whether l > k
@@ -64,32 +62,36 @@ test_sharp_null <- function(df,
 
   # Get hold of the indices
   l_gt_k_inds <- which(l_gt_k_mat)
-  A.shp <- matrix(0, nrow = length(l_gt_k_inds) + K, ncol = len_x)
-  A.shp[cbind(1:length(l_gt_k_inds), l_gt_k_inds)] <- 1
+  
+  # Set shape constraints using A.shp x = beta.shp
+  A.shp <- matrix(0, nrow = K, ncol = len_x)
 
   # Set bound on sum_{l!=k} theta_lk - sum_q delta_qk
+  # Also includes eta_kk (= theta_kk TV_kk) as a "nuisance" parameter
   for (k in 1:K) {
-    A.shp[length(l_gt_k_inds) + k, ((k-1) * K + 1):(k * K)] <- 1
-    A.shp[length(l_gt_k_inds) + k, (k-1) * K + k] <- 0
-    A.shp[length(l_gt_k_inds) + k, par_lengths[1] + ((k-1) * d_y + 1):(k * d_y)] <- -1
-    
-    A.shp[length(l_gt_k_inds) + k, sum(par_lengths[1:2]) + k] <- 1
+    A.shp[k, ((k-1) * K + 1):(k * K)] <- 1
+    A.shp[k, (k-1) * K + k] <- 0
+    A.shp[k, par_lengths[1] + ((k-1) * d_y + 1):(k * d_y)] <- -1
+
+    # Inequalities to equalities
+    A.shp[k, sum(par_lengths[1:2]) + k] <- 1
+    # TV_kk "nuisance" parameters
+    A.shp[k, sum(par_lengths[1:4]) + k] <- 1
   }
-  
-  beta.shp <- rep(0, length(l_gt_k_inds) + K)
+  A.shp <- A.shp[, -l_gt_k_inds]
+  beta.shp <- rep(0, K)
 
 
   # Set remaining constraints using A.obs x = beta.obs
 
   # Constructing beta_obs
-  # WARNING: table may omit rows/columns with 0 observations; to be fixed later
   get_beta.obs <- function(yvec, dvec, mvec) {
 
     # Matrices (with dimension d_y x K) that store P(y,m | d) for d = 0,1
     # d = 0
-    p_ym_d0 <- prop.table(table(yvec[dvec == 0], mvec[dvec == 0]))
+    p_ym_d0 <- prop.table(table(factor(yvec)[dvec == 0], factor(mvec)[dvec == 0]))
     # d = 1
-    p_ym_d1 <- prop.table(table(yvec[dvec == 1], mvec[dvec == 1]))
+    p_ym_d1 <- prop.table(table(factor(yvec)[dvec == 1], factor(mvec)[dvec == 1]))
 
     # Matrices that store P(m | d) for d = 0,1
     # d = 0
@@ -114,7 +116,7 @@ test_sharp_null <- function(df,
 
   # Define A.obs
   A.obs <- matrix(0,
-                  nrow = K + K +  (d_y * K),
+                  nrow = K + K + (d_y * K),
                   ncol = len_x)
   
   for (k in 1:K) { 
@@ -136,19 +138,15 @@ test_sharp_null <- function(df,
     ## A.obs[2 * K + k, sum(par_lengths[1:2]) + k] <- 1
     A.obs[cbind(2 * K + ((k-1) * d_y + 1):(k * d_y),
                 sum(par_lengths[1:3]) + ((k-1) * d_y + 1):(k * d_y))] <- 1
-
-    # Nuisance par to create target
-    A.obs[k, sum(par_lengths[1:4]) + k] <- 1
   }
 
-  # Sanity check
-  ## print(rowSums(A.obs))
-
+  A.obs <- A.obs[,-l_gt_k_inds]
 
   # Define target parameter
   A.tgt <- numeric(len_x)
   A.tgt[sum(par_lengths[1:4]) + (1:K)] <- 1
-
+  A.tgt <- A.tgt[-l_gt_k_inds]
+  
   # Run FSST
   lpm <- lpinfer::lpmodel(A.obs = A.obs,
                           A.shp = A.shp,
