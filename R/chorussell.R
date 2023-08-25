be2c406c3ce1244db8ffeab671d9751ee89f4a02
Soonwoo Kwon <soonwoo.kn@gmail.com>
2023-08-25 10:50:19 -0400

    Started working on Cho & Russell (2023)

---
+++ R/chorussell.R
@@ -0,0 +1,18 @@
+chorussell <- function(df, R, perturb = 1e-3, A.obs, A.shp, A.tgt, beta.obs,
+                       beta.shp, beta.tgt) {
+
+  lbminus <- rep(NA,R)
+  ubminus <- rep(NA,R)
+
+  lbplus <- rep(NA,R)
+  ubplus <- rep(NA,R)
+
+  lbvec <- rep(NA,R)
+  ubvec <- rep(NA,R)
+
+  CSlb5 <- rep(NA,R)
+  CSub5 <- rep(NA,R)
+
+  rej5 <- rep(NA,R)
+  
+}
