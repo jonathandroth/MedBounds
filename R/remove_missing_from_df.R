ce3bbc494526aa87f330da096f84c99d294f906f
Jonathan Roth <jonathan_roth@brown.edu>
2023-08-27 16:33:44 -0400

    Allowing for vector valued m in remove missing

--- R/remove_missing_from_df.R
+++ R/remove_missing_from_df.R
@@ -8,7 +8,7 @@
 remove_missing_from_df <- function(df, d, m, y, w = NULL){
 
   missing_d <- is.na(df[[d]])
-  missing_m <- pmax(is.na(df[[m]])) #allow for vector-valued m
+  missing_m <- !complete.cases(df[,m]) #allow for vector-valued m
   missing_y <- is.na(df[[y]])
 
   df <- df[!(missing_d | missing_m | missing_y), ]
