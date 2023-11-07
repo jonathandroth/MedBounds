6085a4c2b4bac93f7d312d0661c5be7a8bd07e84
Jonathan Roth <jonathan_roth@brown.edu>
2023-11-07 14:39:12 -0500

    removed vira's leebounds package as a dependency, sicne this appears to no longer be online. removed the corresponding tests

--- tests/testthat/test-compute_bounds_ats.R
+++ tests/testthat/test-compute_bounds_ats.R
@@ -1,24 +1,24 @@
-test_that("Lee bounds and ours coincide if don't do correction for point mass", {
-  library(leebounds)
-  leeresults <-
-    leebounds::leebounds(MedBounds::kerwin_data %>%
-                dplyr::rename(outcome = EL_EGRA_PCA_Index,
-                       selection = primarily_leblango,
-                       treat = treated))
-
-  bounds_ats <-
-  compute_bounds_ats(df = kerwin_data,
-                     d= "treated",
-                     m = "primarily_leblango",
-                     y = "EL_EGRA_PCA_Index", adjust_for_point_mass = F)
-  expect_equal(bounds_ats$lb, leeresults$lower_bound,
-               tolerance = 0.003)
-
-  expect_equal(bounds_ats$ub, leeresults$upper_bound,
-               tolerance = 0.003)
-
-})
-
+# test_that("Lee bounds and ours coincide if don't do correction for point mass", {
+#   library(leebounds)
+#   leeresults <-
+#     leebounds::leebounds(MedBounds::kerwin_data %>%
+#                 dplyr::rename(outcome = EL_EGRA_PCA_Index,
+#                        selection = primarily_leblango,
+#                        treat = treated))
+#
+#   bounds_ats <-
+#   compute_bounds_ats(df = kerwin_data,
+#                      d= "treated",
+#                      m = "primarily_leblango",
+#                      y = "EL_EGRA_PCA_Index", adjust_for_point_mass = F)
+#   expect_equal(bounds_ats$lb, leeresults$lower_bound,
+#                tolerance = 0.003)
+#
+#   expect_equal(bounds_ats$ub, leeresults$upper_bound,
+#                tolerance = 0.003)
+#
+# })
+#
 
 
 test_that("Bounds get wider when correcting for point mass", {
